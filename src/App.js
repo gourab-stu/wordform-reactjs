38eec209a45393d35e1be71237efa46562776730
gourab-stu <gourab.acharjee2005@gmail.com>
2024-11-25 00:40:27 +0530

    improved the toggle mode button

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
+import { useState } from "react";
 import "./App.css";
 // import About from "./components/About";
 import Navbar from "./components/Navbar";
 import TextForm from "./components/TextForm";
 
 function App() {
+    const [mode, setMode] = useState("light");
+    const toggleMode = () => {
+        if (mode === "light") {
+            setMode("dark");
+            document.body.style.backgroundColor = "#202c33";
+        } else {
+            setMode("light");
+            document.body.style.backgroundColor = "white";
+        }
+    }
     return (
         <>
-            <Navbar title="TextUtils" aboutText="About"/>
+            <Navbar title="TextUtils" aboutText="About" mode={mode} toggleMode={toggleMode} />
             <div className="container my-3">
-                <TextForm heading="Enter the text to analyze"/>
+                <TextForm heading="Enter the text to analyze" mode={mode} />
                 {/* <About/> */}
             </div>
         </>
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 
 
-export default function TextForm(props) {
+export default function TextForm({mode, heading}) {
     const handleOnUppercase = () => {
         setText(text.toUpperCase());
     };
@@ -30,9 +30,9 @@ export default function TextForm(props) {
     const [text, setText] = useState("Enter text here");
     return (
         <>
-            <h2>{props.heading}</h2>
+            <h2 style={{color: mode==="dark"?"white":"black"}}>{heading}</h2>
             <div className="mb-1">
-                <textarea className="form-control" value={text} onChange={handleOnChange} id="myBox" rows="8"></textarea>
+                <textarea className="form-control" value={text} style={{backgroundColor: mode==="dark"?"#202c33":"white", color: mode==="dark"?"white":"black"}} onChange={handleOnChange} id="myBox" rows="8"></textarea>
             </div>
             <button className="btn btn-primary me-2 mt-2" onClick={handleOnUppercase}>Convert to Uppercase</button>
             <button className="btn btn-primary mx-2 mt-2" onClick={handleOnLowercase}>Convert to Lowercase</button>
@@ -40,10 +40,10 @@ export default function TextForm(props) {
             <button className="btn btn-primary mx-2 mt-2" onClick={handleOnClearText}>Clear Text</button>
             <button className="btn btn-primary mx-2 mt-2" onClick={handleOnCopyText}>Copy Text</button>
             <button className="btn btn-primary mx-2 mt-2" onClick={removeExtraSpaces}>Remove Extra Spaces</button>
-            <h2 className="mt-4">Text Summary</h2>
-            <p> {text.length} characters | {text.split(" ").length - 1} words | {text.split(". ").length - 1} sentences </p>
-            <h2 className="mt-4">Preview</h2>
-            <p> {text} </p>
+            <h2 className="mt-4" style={{color: mode==="dark"?"white":"black"}}>Text Summary</h2>
+            <p style={{color: mode==="dark"?"white":"black"}}> {text.length} characters | {text.split(" ").length} words | {text.split(". ").length - 1} sentences </p>
+            <h2 className="mt-4" style={{color: mode==="dark"?"white":"black"}}>Preview</h2>
+            <p style={{color: mode==="dark"?"white":"black"}}> {text.length>0?text:"Enter your text in the text box to preview here"} </p>
         </>
     );
 }
